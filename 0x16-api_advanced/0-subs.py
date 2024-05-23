5f10354890aa242b82756e89353e028b9cc3fdf3
daniloseibonsu <daniloseibonsu@gmail.com>
2024-05-23 05:17:36 -0700

    sub

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
-""" calls Reddit API and returns the number of subscribers """
+'''
+This function queries the Reddit API and returns
+the number of subscribers
+'''
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """ returns number of subs given a subreddit """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    agt = {"User-Agent": "linux:1:v1.0 (by /u/heimer_r)"}
-    subs = requests.get(url, headers=agt, allow_redirects=False)
-    if subs.status_code != 200:
-        return (0)
-    num_subs = subs.json().get("data").get("subscribers")
-    return (num_subs)
+    '''Returns number of subscribers'''
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'Alx-User'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
