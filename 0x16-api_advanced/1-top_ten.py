1733b15480c9ee6f95aa1d00ee8a701f7f587c45
daniloseibonsu <daniloseibonsu@gmail.com>
2024-05-23 05:53:51 -0700

    ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,26 +1,18 @@
 #!/usr/bin/python3
-'''
-This function queries the Reddit API and prints the
-titles of the first 10 hot posts
-'''
-
-import requests
+"""Module for task 1"""
 
 
 def top_ten(subreddit):
-    '''Returns top ten posts titles'''
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-    headers = {'User-Agent': 'Alx-User'}
-    params = {'limit': 10}
-    response = requests.get(url,
-                            headers=headers,
-                            params=params,
-                            allow_redirects=False)
+    """Queries the Reddit API and returns the top 10 hot posts
+    of the subreddit"""
+    import requests
 
-    if response.status_code == 200:
-        data = response.json()
-        objects = data['data']['children']
-        for obj in objects:
-            print(obj['data']['title'])
-    else:
+    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
         print('None')
+    else:
+        [print(child.get("data").get("title"))
+         for child in sub_info.json().get("data").get("children")]
